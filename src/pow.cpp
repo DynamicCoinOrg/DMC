df6e70e3a34c881de721e2137a6be3a6664e86a1
fourtwentytwo <fourtwentytwo@yandex.com>
2015-04-13 04:34:02 +0300

    Retarget difficulty each block

--- src/pow.cpp
+++ src/pow.cpp
@@ -5,6 +5,9 @@
 
 #include "pow.h"
 
+#include <vector>
+#include <algorithm>
+
 #include "chain.h"
 #include "chainparams.h"
 #include "primitives/block.h"
@@ -13,50 +16,90 @@
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
 {
-    unsigned int nProofOfWorkLimit = Params().ProofOfWorkLimit().GetCompact();
+    const uint256 nProofOfWorkLimit = Params().ProofOfWorkLimit();
+    const unsigned int nProofOfWorkLimitNBits = nProofOfWorkLimit.GetCompact();
 
     // Genesis block
-    if (pindexLast == NULL)
-        return nProofOfWorkLimit;
-
-    if (Params().AllowMinDifficultyBlocks())
-    {
-        // If the new block's timestamp is more than 2* TargetSpacing
-        // then allow mining of a min-difficulty block.
-        if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + Params().TargetSpacing()*2)
-            return nProofOfWorkLimit;
+    if (pindexLast == NULL) {
+        return nProofOfWorkLimitNBits;
     }
 
-    // Only change once per interval
-    if ((pindexLast->nHeight+1) % Params().Interval() != 0)
-        return pindexLast->nBits;
+    if (Params().AllowMinDifficultyBlocks()) {
+        // If the new block's timestamp is more than 2* TargetSpacing
+        // then allow mining of a min-difficulty block.
+        if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + Params().TargetSpacing()*2) {
+            return nProofOfWorkLimitNBits;
+        }
+    }
 
-    // Go back by what we want to be 14 days worth of blocks
+    const int64_t diff_window = Params().Interval();
+    const int64_t diff_timestamp_outlier_cutoff = 60;
+
+    // Go back by `Interval` blocks (Interval = nTargetTimespan / nTargetSpacing)
+    std::vector<int64_t> timestamps;
+    std::vector<uint256> chainWorks;
     const CBlockIndex* pindexFirst = pindexLast;
-    for (int i = 0; pindexFirst && i < Params().Interval()-1; i++)
-        pindexFirst = pindexFirst->pprev;
-    assert(pindexFirst);
+    for (int i = 0; pindexFirst && i < diff_window - 1; i++) {
+        timestamps.push_back(pindexFirst->GetBlockTime());
+        chainWorks.push_back(pindexFirst->nChainWork);
 
-    // Limit adjustment step
-    int64_t nActualTimespan = pindexLast->GetBlockTime() - pindexFirst->GetBlockTime();
-    LogPrintf("  nActualTimespan = %d  before bounds\n", nActualTimespan);
-    if (nActualTimespan < Params().TargetTimespan()/4)
-        nActualTimespan = Params().TargetTimespan()/4;
-    if (nActualTimespan > Params().TargetTimespan()*4)
-        nActualTimespan = Params().TargetTimespan()*4;
+        pindexFirst = pindexFirst->pprev;
+    }
+
+    const size_t length = timestamps.size();
+    assert(length == chainWorks.size());
+    if (length <= 1) {
+        return nProofOfWorkLimitNBits;
+    }
+
+//    static_assert(diff_window >= 2, "Window is too small");
+    assert(length <= diff_window);
+    std::sort(timestamps.begin(), timestamps.end(), std::greater<int64_t>());
+
+    // cutoff timestamp outliers
+    size_t cutoff_begin = length;
+    size_t cutoff_end   = 0;
+
+//    static_assert(2 * diff_timestamp_outlier_cutoff <= diff_window - 2, "Cut length is too large");
+    const int64_t timestamp_core = diff_window - 2 * diff_timestamp_outlier_cutoff;
+    if (length > timestamp_core) {
+        cutoff_begin = (length - timestamp_core + 1) / 2;
+        cutoff_end   = cutoff_begin + timestamp_core;
+    }
+    assert(/*cutoff_begin >= 0 &&*/ cutoff_end + 2 <= cutoff_begin && cutoff_begin <= length);
+
+    int64_t nTargetTimespan = Params().TargetTimespan();
+    int64_t nActualTimespan = timestamps[cutoff_end] - timestamps[cutoff_begin - 1];//pindexLast->GetBlockTime() - pindexFirst->GetBlockTime();
+    uint256 nTotalWork      = chainWorks[cutoff_end] - chainWorks[cutoff_begin - 1];//pindexLast->nChainWork - pindexFirst->nChainWork;
+    LogPrintf("nTargetTimespan = %d\n", nTargetTimespan);
+    LogPrintf("nActualTimespan = %d\n", nActualTimespan);
+    LogPrintf("nTotalWork = %d, nFirstChainWork = %d, nLastChainWork = %d\n",
+               nTotalWork.getdouble(), chainWorks[cutoff_begin - 1].getdouble(),
+                                       chainWorks[cutoff_end].getdouble());
+
+    if (nActualTimespan == 0) {
+        nActualTimespan = 1;
+    }
+    assert(nTotalWork > 0);
 
     // Retarget
-    uint256 bnNew;
-    uint256 bnOld;
-    bnNew.SetCompact(pindexLast->nBits);
-    bnOld = bnNew;
-    bnNew *= nActualTimespan;
-    bnNew /= Params().TargetTimespan();
+    uint256 bnNewHashes(nTotalWork);
+    bnNewHashes *= Params().TargetSpacing();
+    bnNewHashes += nActualTimespan;
+    bnNewHashes -= 1;
+    bnNewHashes /= nActualTimespan;
 
-    if (bnNew > Params().ProofOfWorkLimit())
-        bnNew = Params().ProofOfWorkLimit();
+    uint256 bnNew;
+    if (bnNewHashes <= GetNBitsHashes(nProofOfWorkLimitNBits)) {
+        bnNew = nProofOfWorkLimit;
+    } else {
+        uint256 powPerHash(nProofOfWorkLimit / GetNBitsHashes(nProofOfWorkLimitNBits));
+        bnNew = bnNewHashes * powPerHash - 1;
+    }
 
     /// debug print
+    uint256 bnOld;
+    bnOld.SetCompact(pindexLast->nBits);
     LogPrintf("GetNextWorkRequired RETARGET\n");
     LogPrintf("Params().TargetTimespan() = %d    nActualTimespan = %d\n", Params().TargetTimespan(), nActualTimespan);
     LogPrintf("Before: %08x  %s\n", pindexLast->nBits, bnOld.ToString());
