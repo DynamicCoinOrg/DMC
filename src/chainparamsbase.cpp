18203ba134f6f2c12112ff9679f1be21f15e9517
fourtwentytwo <fourtwentytwo@yandex.com>
2015-04-19 21:53:53 +0300

    Change rpc port numbers

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 8332;
+        nRPCPort = 7332;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 18332;
+        nRPCPort = 17332;
         strDataDir = "testnet3";
     }
 };
